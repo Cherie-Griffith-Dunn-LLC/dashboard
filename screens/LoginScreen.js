dd5ba60d533ce724231340712def68900ed781a9
Mohammed <saleem@simplylion.com>
2023-03-03 10:19:20 -0500

    removed unsused code

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
-import { Text, Layout, Card, Input, Button, Divider, Icon } from '@ui-kitten/components';
+import { Text, Layout, Card, Input, Button, Tooltip, Icon } from '@ui-kitten/components';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as WebBrowser from 'expo-web-browser';
-import { makeRedirectUri, useAuthRequest, useAutoDiscovery, exchangeCodeAsync, AccessTokenRequest } from 'expo-auth-session';
-import { TokenContext } from '../contexts/tokenContext';
-import * as SecureStore from 'expo-secure-store';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getTenantId } from '../services/azureApi';
 
-// authentication via Azure AD
-WebBrowser.maybeCompleteAuthSession();
+
 
 
 const loginHeader = (props) => (
@@ -23,8 +18,6 @@ const loginHeader = (props) => (
     <Icon name='wifi' {...props} />
   );
 
-  // app keys
-  const clientId = '94a4d08f-e078-45f2-a42a-ceb9ad7439ec';
 
   export default function LoginScreen({ navigation }) {
 
