5474acba0a0eb8ea925011e668f85b0570208fd8
Mohammed <saleem@simplylion.com>
2023-03-05 12:43:24 -0500

    added modal for alarms, DWM, and events.

--- components/alarmsList.js
+++ components/alarmsList.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { List, ListItem, Button, Icon } from '@ui-kitten/components';
+import { List, ListItem, Button, Icon, Modal, Text, Card } from '@ui-kitten/components';
 
 const data = new Array(8).fill({
   title: 'Alarm',
   description: 'Alarm Description',
+  priority: 20,
+  priority_label: 'low',
+  rule_intent: 'Environmental Awareness',
+  app_type: 'amazon-aws',
+  source_username: 'user@cgdgovsolutions.com',
+  security_group_id: 'sg-xxxxx',
+  destination_name: 'ec2.amazonaws.com',
+  timestamp_occured: '1517932134000',
+  authentication_type: 'IAMUser',
+  ruled_method: 'AWS EC2 Security Group Modified',
+  app_id: 'amazon-aws',
+  source_name: 'x.xx.xx.xxxx',
+  timestamp_received: '1517933139670',
+  rule_strategy: 'Network Access Control Modification',
+  request_user_agent: 'signin.amazonaws.com',
+  rule_id: 'AWSEC2SecurityGroupMod',
+  sensor_uuid: '433152d2-10ee-4645-8c04-9f8269a447e7',
+  transient: false,
+  event_name: 'Add inbound network traffic rule to security group',
+  status: 'open'
 });
 
 export const DashboardAlarmsList = () => {
+  // control modal visibility
+  const [visible, setVisible] = React.useState(false);
+  // store current data to pass to modal
+  const [currentData, setCurrentData] = React.useState(null);
 
-    const renderItemAccessory = (props) => (
-        <Button {...props} size='tiny'>VIEW</Button>
+    const renderItemAccessory = (props, index) => (
+        <Button {...props} onPress={() => {setVisible(true); setCurrentData(index)}} size='tiny'>VIEW</Button>
     );
 
     const renderItemIcon = (props) => (
@@ -22,20 +46,36 @@ export const DashboardAlarmsList = () => {
     title={`${item.title} ${index + 1}`}
     description={`${item.description}`}
     accessoryLeft={renderItemIcon}
-    accessoryRight={renderItemAccessory} />
+    accessoryRight={(props) => renderItemAccessory(props, index)} />
   );
 
   return (
+    <>
     <List
       style={styles.container}
       data={data}
       renderItem={renderItem}
     />
+    <Modal
+        visible={visible}
+        onBackdropPress={() => setVisible(false)}
+        backdropStyle={styles.backdrop}
+        >
+        <Card>
+          <Text>{data[currentData]?.title} {currentData + 1}</Text>
+          <Text>{data[currentData]?.description}</Text>
+          <Button onPress={() => setVisible(false)}>Close</Button>
+        </Card>
+    </Modal>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    backdrop: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
     }
 });
--- components/dwmList.js
+++ components/dwmList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { List, ListItem, Button, Icon } from '@ui-kitten/components';
+import { List, ListItem, Button, Icon, Modal, Text, Card } from '@ui-kitten/components';
 
 const data = new Array(8).fill({
   title: 'Credentials Leak',
@@ -9,8 +9,13 @@ const data = new Array(8).fill({
 
 export const DWMList = () => {
 
-    const renderItemAccessory = (props) => (
-        <Button {...props} size='tiny'>VIEW</Button>
+  // control modal visibility
+  const [visible, setVisible] = React.useState(false);
+  // store current data to pass to modal
+  const [currentData, setCurrentData] = React.useState(null);
+
+    const renderItemAccessory = (props, index) => (
+        <Button {...props} onPress={() => {setVisible(true); setCurrentData(index)}} size='tiny'>VIEW</Button>
     );
 
     const renderItemIcon = (props) => (
@@ -22,20 +27,36 @@ export const DWMList = () => {
     title={`${item.title} ${index + 1}`}
     description={`${item.description}`}
     accessoryLeft={renderItemIcon}
-    accessoryRight={renderItemAccessory} />
+    accessoryRight={(props) => renderItemAccessory(props, index)} />
   );
 
   return (
+    <>
     <List
       style={styles.container}
       data={data}
       renderItem={renderItem}
     />
+    <Modal
+        visible={visible}
+        onBackdropPress={() => setVisible(false)}
+        backdropStyle={styles.backdrop}
+        >
+        <Card>
+          <Text>{data[currentData]?.title} {currentData + 1}</Text>
+          <Text>{data[currentData]?.description}</Text>
+          <Button onPress={() => setVisible(false)}>Close</Button>
+        </Card>
+    </Modal>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    backdrop: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
     }
 });
