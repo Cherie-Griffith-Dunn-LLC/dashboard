fc54528d863406e8592242eafa344615fcae380a
Mohammed <saleem@simplylion.com>
2023-03-17 13:56:48 -0400

    added logo to LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Platform } from 'react-native';
+import { StyleSheet, View, Platform, ImageBackground, Image } from 'react-native';
 import { Text, Layout, Card, Input, Button, Tooltip, Icon } from '@ui-kitten/components';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { getTenantId } from '../services/azureApi';
@@ -68,28 +68,56 @@ const loginHeader = (props) => (
     );
 
         return (
-            <SafeAreaView style={{ flex: 1}}>
+            <SafeAreaView style={{ flex: 1 }}>
                 <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text category='h1'>Cyproteck</Text>
-                    <Card header={loginHeader}>
-                    <Tooltip
-                    anchor={renderInputEmail}
-                    visible={errorVisible}
-                    placement='top'
-                    onBackdropPress={() => setErrorVisible(false)}>
-                        {error}
-                    </Tooltip>
-                        <Button
-                        title='Login'
-                        disabled={!email}
-                        onPress={() => {
-                            login(email);
-                        }}
-                        >
-                            Login
-                        </Button>
-                    </Card>
+                        <Image source={require('../assets/cyplogo-blk.png')} style={styleguideUIcomponents1Styles.logo} />
+                        <Card header={loginHeader}>
+                        <Tooltip
+                        anchor={renderInputEmail}
+                        visible={errorVisible}
+                        placement='top'
+                        onBackdropPress={() => setErrorVisible(false)}>
+                            {error}
+                        </Tooltip>
+                            <Button
+                            title='Login'
+                            disabled={!email}
+                            onPress={() => {
+                                login(email);
+                            }}
+                            >
+                                Login
+                            </Button>
+                        </Card>
                 </Layout>
             </SafeAreaView>
           );
     };
+
+
+    const styleguideUIcomponents1Styles = StyleSheet.create({
+        Input: {
+            color: '#d9e1e7',
+            fontFamily: 'Proxima Nova, sans-serif',
+            fontSize: 1,
+            lineHeight: 1.22,
+          },
+          Button: {
+            margin: '3.94 1.31',
+            height: '33.33%',
+            color: '#ffffff',
+            fontFamily: 'Proxima Nova, sans-serif',
+            fontSize: 1.13,
+            lineHeight: 1.37,
+          },
+          BackImage: {
+            flex: 1,
+            justifyContent: 'center',
+            resizeMode: 'cover'
+          },
+          logo: {
+            height: '40px',
+            width: '265px'
+          }
+      })
+      
