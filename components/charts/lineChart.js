ec5d2d3cbd2af22c0cc6f64236677dbc8c47ec3e
Mohammed <saleem@simplylion.com>
2023-03-18 15:54:56 -0400

    matched line chart to ui reference

--- components/charts/lineChart.js
+++ components/charts/lineChart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { LineChart, XAxis } from 'react-native-svg-charts';
 import * as shape from 'd3-shape';
+import { Circle, Path } from 'react-native-svg';
 
 export default class CustomLineChart extends React.Component {
 
@@ -9,12 +10,12 @@ export default class CustomLineChart extends React.Component {
     render() {
 
         // fake data
-        const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+        const oldData = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
 
         
         // check if data is empty
         if (!this.props.data) {
-            var newData = data;
+            var newData = oldData;
         } else {
             //var newData = this.props.data;
             // parse data to get how many occured on each day then push to newData
@@ -47,7 +48,34 @@ export default class CustomLineChart extends React.Component {
             // sort newData by date
             newData.sort((a, b) => (a.date > b.date) ? 1 : -1);
         }
+        // chart line
+        const Line = ({ line }) => (
+            <Path
+                d={ line }
+                stroke={ 'rgb(0, 144, 255)' }
+                strokeWidth="3"
+                fill={ 'none' }
+            />
+        )
+        // line decorator
+        const Decorator = ({ x, y, data }) => {
+            return data.map((value, index) => (
+                <Circle
+                    key={ index }
+                    cx={ x(value.date) }
+                    cy={ y(value.occurences) }
+                    r={ 4 }
+                    strokeWidth="2.5"
+                    stroke={ 'white' }
+                    fill={ 'rgb(0, 144, 255)' }
+                />
+            ))
+        }
+
+
         const contentInset = { top: 20, bottom: 20 }
+
+        
 
         return (
             <>
@@ -59,9 +87,10 @@ export default class CustomLineChart extends React.Component {
                 xMin={0}
                 xMax={24}
                 curve={ shape.curveNatural }
-                svg={{ stroke: 'rgb(0, 144, 255)', strokeWidth: "5" }}
                 contentInset={contentInset}
             >
+                <Line />
+                <Decorator />
             </LineChart>
             <XAxis
                 data={newData}
