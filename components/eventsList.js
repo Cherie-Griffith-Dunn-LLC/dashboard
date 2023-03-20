09bdbbde5233590a11195a39bebc49b8fe2c53c2
Mohammed <saleem@simplylion.com>
2023-03-20 18:25:35 -0400

    improved event cards & added icons,info output

--- components/eventsList.js
+++ components/eventsList.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { List, ListItem, Button, Icon, Modal, Text, Card, Divider } from '@ui-kitten/components';
 // usm api function
 import { getEvents } from '../services/usmApi';
 import GlobalStyles from '../constants/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
 
 
 export const DashboardEventsList = (props) => {
@@ -14,10 +17,11 @@ export const DashboardEventsList = (props) => {
   const [loading, setLoading] = React.useState(true);
   // get alarms from api
   React.useEffect(() => {
-    getEvents(props.token).then((response) => {
+    getEvents(props.token, 20).then((response) => {
       console.log(response);
       // replace empty data array with response data
       setData(response._embedded.eventResources);
+      console.log(data);
       setLoading(false);
     });
   }, []);
@@ -25,7 +29,7 @@ export const DashboardEventsList = (props) => {
   // control modal visibility
   const [visible, setVisible] = React.useState(false);
   // store current data to pass to modal
-  const [currentData, setCurrentData] = React.useState(null);
+  const [currentData, setCurrentData] = React.useState([]);
 
   const buttonArrow = (props) => (
     <Icon {...props} name='arrow-ios-forward-outline' />
@@ -34,21 +38,42 @@ export const DashboardEventsList = (props) => {
 
   const renderItemAccessory = (props, index) => (
     <Button style={GlobalStyles.button}
-    {...props} onPress={() => {setVisible(true); setCurrentData(index)}}
+    {...props} onPress={() => {setVisible(true); setCurrentData(data[index])}}
     accessoryLeft={buttonArrow}
     size='medium' status='basic'></Button>
 );
 
     const renderItemIcon = (props) => (
-        <Icon {...props} name='question-mark-circle-outline' />
+      <FontAwesomeIcon {...props} icon={faCircleQuestion} />
     );
 
+    const securityItemIcon = (props) => (
+      <FontAwesomeIcon {...props} icon={faShieldHalved} />
+  );
+ 
   const renderItem = ({ item, index }) => (
     <ListItem
+    onPress={() => {setVisible(true); setCurrentData(data[index])}}
     title={`${item.event_name}`}
-    description={`${item.event_category ? item.event_category : item.event_type}`}
-    accessoryLeft={renderItemIcon}
+    description={`${item.event_type ? item.event_type : item.event_category}`}
+    accessoryLeft={
+      item.event_category === 'Security' ? securityItemIcon : (
+        renderItemIcon
+      )}
     accessoryRight={(props) => renderItemAccessory(props, index)} />
+  );
+
+  const cardHeader = (props) => (
+    <View {...props} style={[props.style, styles.headerContainer]}>
+      <Text category='h6'>{currentData?.event_name}</Text>
+      <Text category='s1'>{currentData?.plugin_device_type}</Text>
+    </View>
+  );
+
+  const cardFooter = (props) => (
+    <View {...props} style={[props.style, styles.cardFooter]}>
+      <Button style={[GlobalStyles.button, styles.button]} onPress={() => setVisible(false)}>Close</Button>
+    </View>
   );
 
   // if loading data, show loading text
@@ -76,45 +101,19 @@ export const DashboardEventsList = (props) => {
         onBackdropPress={() => setVisible(false)}
         backdropStyle={styles.backdrop}
         >
-        <Card>
-          <Text>Title: {data[currentData]?.event_name}</Text>
-          <Text>Description: {data[currentData]?.event_description}</Text>
-          <Text>Account Name: {data[currentData]?.account_name}</Text>
-          <Text>Plugin Device Type: {data[currentData]?.plugin_device_type}</Text>
-          <Text>Destination Canonical: {data[currentData]?.destination_canonical}</Text>
-          <Text>Destination Name: {data[currentData]?.destination_name}</Text>
-          <Text>Has Alarm: {data[currentData]?.has_alarm}</Text>
-          <Text>Request User Agent: {data[currentData]?.request_user_agent}</Text>
-          <Text>Packet Type: {data[currentData]?.packet_type}</Text>
-          <Text>Source Canonical: {data[currentData]?.source_canonical}</Text>
-          <Text>Event Name: {data[currentData]?.event_name}</Text>
-          <Text>Timestamp Occured: {data[currentData]?.timestamp_occured}</Text>
-          <Text>Source Service Name: {data[currentData]?.source_service_name}</Text>
-          <Text>Event Type: {data[currentData]?.event_type}</Text>
-          <Text>App Name: {data[currentData]?.app_name}</Text>
-          <Text>Timestamp Received: {data[currentData]?.timestamp_received}</Text>
-          <Text>Destination Hostname: {data[currentData]?.destination_hostname}</Text>
-          <Text>Source Infrastructure Name: {data[currentData]?.source_infrastructure_name}</Text>
-          <Text>Plugin: {data[currentData]?.plugin}</Text>
-          <Text>App Type: {data[currentData]?.app_type}</Text>
-          <Text>Authentication Type: {data[currentData]?.authentication_type}</Text>
-          <Text>Access Control Outcome: {data[currentData]?.access_control_outcome}</Text>
-          <Text>Suppressed: {data[currentData]?.suppressed}</Text>
-          <Text>Plugin Device: {data[currentData]?.plugin_device}</Text>
-          <Text>Destination Infrastructure Type: {data[currentData]?.destination_infrastructure_type}</Text>
-          <Text>Source Infrastructure Type: {data[currentData]?.source_infrastructure_type}</Text>
-          <Text>Destination Zone: {data[currentData]?.destination_zone}</Text>
-          <Text>Needs Enrichment: {data[currentData]?.needs_enrichment}</Text>
-          <Text>Source Hostname: {data[currentData]?.source_hostname}</Text>
-          <Text>App ID: {data[currentData]?.app_id}</Text>
-          <Text>Plugin Family: {data[currentData]?.plugin_family}</Text>
-          <Text>Plugin Version: {data[currentData]?.plugin_version}</Text>
-          <Text>Destination User ID: {data[currentData]?.destination_userid}</Text>
-          <Text>Event Action: {data[currentData]?.event_action}</Text>
-          <Text>Destination Infrastructure Name: {data[currentData]?.destination_infrastructure_name}</Text>
-          <Text>Source Name: {data[currentData]?.source_name}</Text>
-          <Text>Received From: {data[currentData]?.received_from}</Text>
-          <Button style={GlobalStyles.button} onPress={() => setVisible(false)}>Close</Button>
+        <Card
+          header={cardHeader}
+          footer={cardFooter}
+          status={currentData?.event_severity === 'WARNING' ? 'danger' : (
+            currentData?.event_severity === 'Informational' ? 'info' : (
+              (currentData?.event_severity === 'ERROR' || currentData?.event_severity === 'UNDEFINED') ? 'warning' : 'basic'
+            )
+          )}
+        >
+          <Text>{Math.round((new Date().getTime() - new Date(Math.round(currentData?.timestamp_occured))) / (1000 * 3600 * 24))} days ago.</Text>
+          <Text>Event Type: {currentData?.event_type}</Text>
+          <Text>Event Source: {currentData?.source_name ? currentData?.source_name : currentData?.source_username}</Text>
+          <Text>Packet Type: {currentData?.packet_type}</Text>
         </Card>
     </Modal>
   </>
@@ -124,6 +123,10 @@ export const DashboardEventsList = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    button: {
+      width: '140px',
+      height: '25px'
     },
     backdrop: {
       backgroundColor: 'rgba(0, 0, 0, 0.5)',
