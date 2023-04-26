9c031ca8c61ebb3950aa3933807c6f216ba4b69e
Mohammed <saleem@simplylion.com>
2023-04-26 16:50:29 -0400

    added shadow to cards

--- constants/styles.js
+++ constants/styles.js
@@ -13,8 +13,15 @@ export default StyleSheet.create({
     },
     card: {
         borderRadius: 15,
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 15px 0 rgba(0, 0, 0, 0.19)',
     },
     bgGray: {
         backgroundColor: '#CED1D5'
+    },
+    divider: {
+        width: '100%',
+        color: '#CED1D5',
+        marginTop: 10,
+        marginBottom: 10,
     }
 });
