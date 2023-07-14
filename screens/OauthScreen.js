bd52578723448a58715238d9941caf145c21a2f4
Mohammed <saleem@simplylion.com>
2023-07-14 15:12:44 -0400

    migrate clientID to new tenant app

--- screens/OauthScreen.js
+++ screens/OauthScreen.js
@@ -32,7 +32,7 @@ const loginHeader = (props) => (
   );
 
   // app keys
-  const clientId = '94a4d08f-e078-45f2-a42a-ceb9ad7439ec';
+  const clientId = '1d40f6b3-9072-4a0a-af48-6e423e58d0d6';
 
   export default function OauthScreen({ route, navigation }) {
 
