4fe9eb84c9c6c534285873e811a0c523f008cf11
Mohammed <saleem@simplylion.com>
2023-10-14 12:49:03 -0400

    added api url to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  API_URL: process.env.NODE_ENV === "production" ? "https://cyproteckapi.azurewebsites.net" : "http://localhost:3000",
   google: {
     API_KEY: "",
     CLIENT_ID: "23144678283-oek7ncjmmrgkgmi2i56sc411gp71a8sp.apps.googleusercontent.com",
