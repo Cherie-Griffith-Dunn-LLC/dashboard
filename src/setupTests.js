43cbfb30a8172ee1b44bba20e1f619380df8fd2c
Mohammed <saleem@simplylion.com>
2023-10-14 13:21:35 -0400

    fixed watchMedia error in test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// fix for "matchMedia not present, legacy browsers require a polyfill jest" error
+// https://github.com/akiran/react-slick/blob/master/test-setup.js
+window.matchMedia =
+    window.matchMedia ||
+    function () {
+        return {
+        matches: false,
+        addListener: function () { },
+        removeListener: function () { }
+        };
+    };
