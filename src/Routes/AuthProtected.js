da3d961eee08248748b0ce87cc42aaefc2065971
Mohammed <saleem@simplylion.com>
2023-10-16 17:58:18 -0400

    fix expired token not being logged out

--- src/Routes/AuthProtected.js
+++ src/Routes/AuthProtected.js
@@ -8,14 +8,21 @@ const AuthProtected = (props) => {
   // get the authToken and expiretime from localstorage
   const authToken = localStorage.getItem("accessToken");
   const expireTime = localStorage.getItem("expireTime");
+  console.log("Expire Time", expireTime);
+  console.log("Current Time", Math.floor(Date.now() / 1000));
+  console.log(expireTime < Math.floor(Date.now() / 1000))
 
 
   // check if authToken exist and expireTime is valid
-  if (!authToken && loading) {
+  if (!authToken) {
     return (
       <Navigate to={{ pathname: "/login", state: { from: props.location } }} />
     );
-  } else if (expireTime < Math.floor(Date.now() / 1000) && loading) {
+  } else if (expireTime < Math.floor(Date.now() / 1000)) {
+    // delete the authToken and expireTime if expired
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("expireTime");
+    localStorage.removeItem("authUser");
     return (
       <Navigate to={{ pathname: "/login", state: { from: props.location } }} />
     );
