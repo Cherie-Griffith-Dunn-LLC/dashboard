3195a9ca2f63b6d6a7f6803b31ca8d4c79ef7fb8
Mohammed <saleem@simplylion.com>
2023-10-20 21:57:56 -0400

    added helper to add icons to threats

--- src/helpers/data_helper.js
+++ src/helpers/data_helper.js
@@ -1,5 +1,5 @@
 // returns time since timestamp in human readable form
-const daysAgo = (date) => {
+function daysAgo (date) {
     // Math.round((new Date().getTime() - new Date(Math.round(currentData?.timestamp_occured))) / (1000 * 3600 * 24))
     const days = Math.round((new Date().getTime() - new Date(Math.round(date))) / (1000 * 3600 * 24));
     if (days === 0) {
@@ -10,6 +10,44 @@ const daysAgo = (date) => {
     }
   };
 
+
+  // give an icon based on the rule intent
+  function getAlarmIcon (threats = []) {
+    return threats.map(alarm => {
+      if (alarm.rule_intent === "System Compromise") {
+        return {
+          ...alarm,
+          icon: "mdi mdi-biohazard",
+        }
+      } else if (alarm.rule_intent === "Environmental Awareness") {
+        return {
+          ...alarm,
+          icon: "mdi mdi-web",
+        }
+      } else if (alarm.rule_intent === "Exploit & Installation") {
+        return {
+          ...alarm,
+          icon: "mdi mdi-alarm",
+        }
+      } else if (alarm.rule_intent === "Delivery & Attack") {
+        return {
+          ...alarm,
+          icon: "mdi mdi-crosshairs-gps",
+        }
+      } else if (alarm.rule_intent === "Reconnaisannce & Probing") {
+        return {
+          ...alarm,
+          icon: "mdi mdi-radar",
+        }
+      } else {
+        return {
+          ...alarm
+        }
+      }
+    })
+  }
+
 export {
-    daysAgo
+    daysAgo,
+    getAlarmIcon
 }
